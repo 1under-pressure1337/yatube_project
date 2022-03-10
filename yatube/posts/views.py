c88890da02095a0e1e090a7388656633134ed8a8
Alex Shinko <alexandr_shinko96@mail.ru>
2022-03-10 13:48:27 +0300

    Added urls and view-functions

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+# Главная страница
+
+
+def index(request):
+    return HttpResponse('Главная страница')
+
+
+# Страница с постами, отфильтрованными по группе
+def group_posts(request, any_slug):
+    return HttpResponse('Список постов')
